62e059a686f36c4e2411615b54a6d5b6bae030cd
bob-bins <mchen300@gmail.com>
2023-02-11 16:16:59 -0800

    update practices

--- src/frontPage/myPractices.ts
+++ src/frontPage/myPractices.ts
@@ -13,37 +13,34 @@ export const myPractices = () =>
     div({ class: "container text-center" }, [h1({}, text("Practices"))]),
     div({ class: "container-fluid d-flex flex-row flex-wrap justify-content-center" }, [
       practicesCard(
-        i({ class: "fas fa-shapes mr-1" }, []),
+        i({ class: "fa-solid fa-shapes mr-1" }, []),
         "Purpose-driven decision-making",
         `My decisions are rooted in core principles that make sense to both the team and the business.
         Decisions are never made without proper context, and always void of ad-hoc reasoning.`
       ),
       practicesCard(
-        i({ class: "fas fa-hand-holding-heart mr-1" }, []),
+        i({ class: "fa-solid fa-brain mr-1" }, []),
+        "Challenge the status quo",
+        `The idea behind "If it ain't broke, don't fix it" is often rooted in fear of change, and thus
+        often in opposition to innovation.`
+      ),
+      practicesCard(
+        i({ class: "fa-solid fa-hand-holding-heart mr-1" }, []),
         "Understanding through empathy",
         `Empathy is a must for understanding others' perspectives, which is a prerequisite to 
         building both a product the user loves and a team that loves to build it.`
       ),
       practicesCard(
-        i({ class: "fas fa-book-open mr-1" }, []),
+        i({ class: "fa-solid fa-book-open" }, []),
         "Lifelong student and teacher",
-        `To stop learning is to stop growing, and one of the best ways to learn is to teach.`
+        `To stop learning is to stop growing.
+        And one of the most fulfilling ways to use knowledge is to share it!`
       ),
       practicesCard(
-        div({ class: "text-monospace", style: { "font-weight": "bold" } }, text("f(x)")),
-        "Declarative and typed code",
-        `I embrace functional programming styles with expressive static types. 
-        These higher levels of abstraction increase code discoverability, 
-        increases readability of existing code, and allows entire classes of errors to be 
-        caught as early as possible.`
-      ),
-      practicesCard(
-        i({ class: "fas fa-code mr-1" }, []),
-        "Infrastructure as real code",
-        `IaC has become a popular process of managing infrastructure, but 
-        many tools make use of templating languages that do not allow advanced abstractions.
-        I strive for declarative infrastructure using the full power of 
-        general-purpose languages whenever possible.`
+        i({ class: "fa-solid fa-cubes-stacked mr-1" }, []),
+        "Build for maintenance",
+        `Bad code must still be read many times throughout its lifetime, becoming a significant source of hidden costs.
+        I always strive for intuitive and self-discoverable implementations.`
       ),
     ]),
   ])
